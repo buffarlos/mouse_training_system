e4ad1e66ad20f1d7b31e3a005bac05d1f83e6ef0
Carlos Takeshita <ctakeshita@ucsd.edu>
2025-03-08 12:34:46 -0800

    Training Function Development Progress
    - Added training functions
    - Enabled training function invocation via MQTT
    - Continuous ping to central computer when training not in progress

--- training_system/esp32_firmware/training_system_firmware/training_system_firmware.ino
+++ training_system/esp32_firmware/training_system_firmware/training_system_firmware.ino
@@ -12,7 +12,7 @@
 #include <PubSubClient.h>
 
 // ------------------------- Pin name definitions -------------------------
-// Library only supports hardware SPI at this time
+// Touchscreen library only supports hardware SPI at this time
 // Connect SCLK to ESP32-PROS3 Digital #36 (Hardware SPI clock)
 // Connect MISO to ESP32-PROS3 Digital #37 (Hardware SPI MISO)
 // Connect MOSI to ESP32-PROS3 Digital #35 (Hardware SPI MOSI)
@@ -24,27 +24,49 @@
 #define MAG_REP 42
 
 // ------------------------- Constants -------------------------
+// 5 choice location and size information
 const int choiceXPos[5] = {50, 200, 350, 500, 650};
 const int choiceYPos = 330;
 const int choiceSize = 100;
-// Uncomment and fill with information before flashing
-// const char* systemid = "mouse_1";
+// Reward parameters
+const int pumpFlowRate = 1; // milliliters per second
+const int rewardVolume = 2; // milliliters
+/*
+###################################################
+### Network params - fill with info, then flash ###
+###################################################
+*/
+// const char* subjectID = "";
 // const char* ssid = "";
 // const char* password = "";
-// const char* mqtt_server = "";
+// const char* mqttServer = "";
 
 // ------------------------- Global variables -------------------------
-char mac[17];
+// Touchscreen
 Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS, RA8875_RESET);
 uint16_t tx, ty;
+// Network
+char topicSub[30];
+char topicPub[30];
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
+char outgoingMsg[100];
 int value = 0;
+unsigned long lastCentralComputerPing = 0;
 
 // ------------------------- Helper functions -------------------------
 // Touchscreen helpers
+/**
+  * @brief Illuminates touchscreen choice passed to this function
+  *
+  * Given a 5 choice selection passed to this function, the function will
+  * illuminate it on the touchscreen based on defined 5 choice position
+  * parameters.
+  *
+  * @param choice: Which 5 choice selection to illuminate on screen
+  */
 void illuminateChoice(int choice) {
   tft.fillRect(
     choiceXPos[choice],
@@ -55,6 +77,20 @@ void illuminateChoice(int choice) {
   );
 }
 
+/**
+  * @brief Checks if touchscreen choice passed to this function is touched
+  * based on touch coordinates passed
+  *
+  * Given a 5 choice selection passed to this function, the function will
+  * check whether it has been touched based on defined 5 choice position
+  * parameters and touch coordinates.
+  *
+  * @param x: Touch x coordinate
+  * @param y: Touch y coordinate
+  * @param choice: Which 5 choice selection to check if touched
+  *
+  * @return true if choice touched, false otherwise
+  */
 bool checkChoiceTouch(int x, int y, int choice) {
   float xScale = 1024.0F/tft.width();
   float yScale = 1024.0F/tft.height();
@@ -65,6 +101,12 @@ bool checkChoiceTouch(int x, int y, int choice) {
   return false;
 }
 
+/**
+  * @brief Extinguishes all 5 choices
+  *
+  * This function extinguishes all 5 choices on the touchscreen by drawing over them
+  * with black squares.
+  */
 void extinguishChoices() {
   for (int i = 0; i < 5; ++i) {
     tft.fillRect(
@@ -88,15 +130,20 @@ void printToScreen(int startx, int starty, char text[], uint16_t color, uint16_t
 }
 
 // MQTT helpers
+/**
+  * @brief Attempts to connect to MQTT network if not connected
+  *
+  * This function will attempt to connect to the MQTT network if currently disconnected.
+  */
 void reconnect() {
-  // Loop until we're reconnected
+  // Loop until reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
     if (client.connect("ESP8266Client")) {
       Serial.println("connected");
       // Subscribe
-      client.subscribe("esp32/output");
+      client.subscribe(topicSub);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -107,37 +154,272 @@ void reconnect() {
   }
 }
 
+/**
+  * @brief Interprets incoming MQTT messages
+  *
+  * This function is invoked when an MQTT message arrives on the subscribed topic.
+  * In turn, this function invokes the appropriate training function based on the
+  * message contents.
+  */
 void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.print(topic);
-  Serial.print(". Message: ");
   String messageTemp;
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
-  }
-  Serial.println();
-
-  if (String(topic) == "esp32/output") {
-    Serial.print("Received command ");
-    if(messageTemp == "on"){
-      Serial.println("on");
-      illuminateChoice(2);
+  if (String(topic) == topicSub) {
+    Serial.print("Message arrived on topic: ");
+    Serial.print(topic);
+    Serial.print(". Message: ");
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)message[i]);
+      messageTemp += (char)message[i];
     }
-    else if(messageTemp == "off"){
-      Serial.println("off");
-      extinguishChoices();
+    Serial.println();
+    // Place all central computer commands here!
+    if (messageTemp == "hab1") {
+      hab1();
+    }
+    else if (messageTemp == "hab2") {
+      hab2();
+    }
+    else if (messageTemp == "5csr_citi_10") {
+      fiveChoice(10000, 4000);
+    }
+    else if (messageTemp == "5csr_citi_8") {
+      fiveChoice(8000, 4000);
+    }
+    else if (messageTemp == "5csr_citi_4") {
+      fiveChoice(4000, 4000);
+    }
+    else if (messageTemp == "5csr_citi_2") {
+      fiveChoice(2000, 4000);
+    }
+    else if (messageTemp == "5csr_viti") {
+      unsigned long interTrialDuration = random(2, 7);
+      interTrialDuration = interTrialDuration*1000;
+      fiveChoice(2000, interTrialDuration);
     }
   }
 }
 
 // ------------------------- Training functions -------------------------
+/**
+  * @brief Operates the feeder magazine
+  *
+  * When invoked, this function checks the reward parameter to determine whether
+  * a food reward should be administered. Then, it will illuminate the magazine to
+  * indicate that it is ready for test subject input. Once an input is received,
+  * this function will return the time it took for the subject to provide an input.
+  *
+  * @param reward: Administers reward before illuminating magazine if true,
+  * illuminates magazine without administering reward otherwise
+  */
+unsigned long magOp(bool reward) {
+  // Actuates feeder pump, then illuminates and monitors magazine for reward acceptance.
+  // Returns reward acceptance latency.
+  unsigned long rewardLatency;
+  Serial.println("Entered mag op");
+  if (reward) {
+    Serial.println("Administering reward");
+    digitalWrite(FEED_OP, HIGH);
+    delay(2000); // TODO: calculate based on pump flow rate, desired reward volume
+    digitalWrite(FEED_OP, LOW);
+  }
+  digitalWrite(MAG_OP, HIGH);
+  unsigned long start = millis();
+  while (true) {
+    if (digitalRead(MAG_REP) == LOW) {
+      rewardLatency = millis() - start;
+      digitalWrite(MAG_OP, LOW);
+      break;
+    }
+  }
+  return rewardLatency;
+}
+
+/**
+  * @brief Inter-trial period
+  *
+  * Enforces inter-trial period. When invoked, this function enforces
+  * an inter-trial period during which the subject should refrain from touching
+  * the touchscreen. The function exits when the subject succeeds in not
+  * touching the touchscreen for the period passed in parameter duration.
+  * Touching the touchscreen during the inter-trial period resets the time.
+  *
+  * @param duration: Inter-trial period in milliseconds
+  */
+void interTrialPeriod(unsigned long duration) {
+  bool interTrialPeriodComplete = false;
+  unsigned long start;
+  while (!interTrialPeriodComplete) {
+    tft.touchRead(&tx, &ty);
+    start = millis();
+    while (true) {
+      if (! digitalRead(RA8875_INT)) {
+        if (tft.touched()) {
+          tft.touchRead(&tx, &ty);
+          unsigned long latency = millis() - start;
+          break;
+        }
+      }
+      if (millis() - start > duration) {
+        interTrialPeriodComplete = true;
+        break;
+      }
+      delay(10);
+    }
+  }
+  tft.touchRead(&tx, &ty);
+}
+
+/**
+  * @brief TASK - Habituation 1
+  *
+  * Administers task: Habituation 1
+  * A reward is administered, and the magazine is illuminated. 
+  *
+  * @param duration: Inhibition period in milliseconds
+  */
+void hab1() {
+  unsigned long rewardLatency = magOp(true);
+  sprintf(outgoingMsg, "%d", rewardLatency);
+  client.publish(topicPub, outgoingMsg);
+  interTrialPeriod(4000);
+}
+
+/**
+  * @brief TASK - Habituation 2
+  *
+  * Administers task: Habituation 2
+  * All 5 choices are illuminated. When the mouse touches any choice,
+  * a reward is administered, and the magazine is illuminated. 
+  *
+  * @param duration: Inhibition period in milliseconds
+  */
+void hab2() {
+  unsigned long touchLatency = 0;
+  unsigned long rewardLatency = 0;
+  tft.touchRead(&tx, &ty);
+  for (int i = 0; i < 5; ++i) {
+    illuminateChoice(i);
+  }
+  unsigned long start = millis();
+  while (true) {
+    if (! digitalRead(RA8875_INT)) {
+      if (tft.touched()) {
+        tft.touchRead(&tx, &ty);
+        touchLatency = millis() - start;
+        extinguishChoices();
+        rewardLatency = magOp(true);
+        break;
+      }
+    }
+  }
+  sprintf(outgoingMsg, "%d %d", touchLatency, rewardLatency);
+  client.publish(topicPub, outgoingMsg);
+  interTrialPeriod(4000);
+}
+
+/**
+  * @brief TASK - 5 Choice
+  *
+  * Administers tasks:
+  * 5 CSR CITI
+  * 5 CSR VITI
+  * RCPT
+  * A randomly chosen choice is illuminated for a period specified by parameter
+  * stimulusDuration. The subject's response (positive, negative, absent) is noted,
+  * and the magazine is operated accordingly. An inter-trial period specified by
+  * parameter interTrialDuration is enforced. Parameter variations allow this
+  * function to execute 5 CSR CITI, 5 CSR VITI, and RCPT.
+  *
+  * @param duration: Inhibition period in milliseconds
+  */
+void fiveChoice(unsigned long stimulusDuration, unsigned long interTrialDuration) {
+  unsigned long touchLatency = 0;
+  unsigned long rewardLatency = 0;
+  int choice = random(0, 5);
+  tft.touchRead(&tx, &ty);
+  unsigned long start = millis();
+  illuminateChoice(choice);
+  while (true) {
+    if (! digitalRead(RA8875_INT)) {
+      if (tft.touched()) {
+        tft.touchRead(&tx, &ty);
+        touchLatency = millis() - start;
+        extinguishChoices();
+        if (checkChoiceTouch(tx, ty, choice)) {
+          Serial.print("Positive input in ");
+          Serial.print(touchLatency);
+          Serial.println(" ms");
+          rewardLatency = magOp(true);
+          break;
+        }
+        else {
+          Serial.print("Negative input in ");
+          Serial.print(touchLatency);
+          Serial.println(" ms");
+          rewardLatency = magOp(false);
+          break;
+        }
+      }
+    }
+    if (millis() - start > stimulusDuration) {
+      extinguishChoices();
+      Serial.println("Absent response");
+      rewardLatency = magOp(false);
+      break;
+    }
+    delay(10);
+  }
+  sprintf(outgoingMsg, "%d %d", touchLatency, rewardLatency);
+  client.publish(topicPub, outgoingMsg);
+  interTrialPeriod(interTrialDuration);
+}
+
+/**
+  * @brief TASK - Inhibition
+  *
+  * Administers task: Inhibition.
+  * No choices are illuminated. If the subject refrains from touching the screen
+  * for the period passed in parameter duration, a reward is administered.
+  * If the subject touches the screen during the inhibition period,
+  * no reward is administered, and the latency is noted.
+  * Trial information is transmitted via MQTT.
+  *
+  * @param duration: Inhibition period in milliseconds
+  */
+void inhibitionTrial(unsigned long duration) {
+  tft.touchRead(&tx, &ty);
+  unsigned long start = millis();
+  while (true) {
+    if (! digitalRead(RA8875_INT)) {
+      if (tft.touched()) {
+        tft.touchRead(&tx, &ty);
+        unsigned long latency = millis() - start;
+        Serial.print("Failed inhibition in ");
+        Serial.print(latency);
+        Serial.println(" milliseconds");
+        magOp(false);
+        tft.touchRead(&tx, &ty);
+        return;
+      }
+    }
+    if (millis() - start > duration) {
+      Serial.println("Inhibition success");
+      magOp(true);
+      tft.touchRead(&tx, &ty);
+      ;
+    }
+    delay(10);
+  }
+}
 
 // ------------------------- Setup function -------------------------
 void setup() {
   // Serial diagnostic communication begin
   Serial.begin(9600);
+
+  // MQTT topics
+  sprintf(topicSub, "%s/stage", subjectID);
+  sprintf(topicPub, "%s/data", subjectID);
 
   // Screen initialization
   if (!tft.begin(RA8875_800x480)) {
@@ -145,7 +427,7 @@ void setup() {
     while (1);
   }
   tft.displayOn(true);
-  tft.GPIOX(true);      // Enable TFT - display enable tied to GPIOX
+  tft.GPIOX(true); // Enable TFT - display enable tied to GPIOX
   tft.PWM1config(true, RA8875_PWM_CLK_DIV1024); // PWM output for backlight
   tft.PWM1out(255);
   tft.fillScreen(RA8875_BLACK);
@@ -173,8 +455,13 @@ void setup() {
     Serial.println(WiFi.localIP());
 
   // MQTT initialization
-  client.setServer(mqtt_server, 1883);
+  client.setServer(mqttServer, 1883);
   client.setCallback(callback);
+
+  // GPIO initialization
+  pinMode(FEED_OP, OUTPUT);
+  pinMode(MAG_OP, OUTPUT);
+  pinMode(MAG_REP, INPUT);
 }
 
 // ------------------------- Main loop -------------------------
@@ -183,10 +470,9 @@ void loop() {
     reconnect();
   }
   client.loop();
-
-  long now = millis();
-  if (now - lastMsg > 500) {
-    lastMsg = now;
-    client.publish("esp32/test", "ESP32 alive on network");
+  if (millis() - lastCentralComputerPing > 500) {
+    lastCentralComputerPing = millis();
+    sprintf(outgoingMsg, "ping");
+    client.publish(topicPub, outgoingMsg);
   }
 }
